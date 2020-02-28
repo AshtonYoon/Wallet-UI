9f6c382da4fde7a0ae984ac228f8463e15f62e59
Ashton Yoon <y109384@naver.com>
2020-02-28 15:04:12 +0900

    Change to scrollable content

--- app/src/main/java/com/ashton/wallet/TransferViewHolder.java
+++ app/src/main/java/com/ashton/wallet/TransferViewHolder.java
@@ -5,14 +5,18 @@ import android.content.res.Resources;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 public class TransferViewHolder extends RecyclerView.ViewHolder {
     private final AppCompatTextView mType;
     private final AppCompatTextView mDate;
     private final AppCompatTextView mAmount;
     private final AppCompatTextView mDollar;
+    private final AppCompatImageView mIcon;
 
     private Resources mResources;
 
@@ -23,6 +27,7 @@ public class TransferViewHolder extends RecyclerView.ViewHolder {
         mDate = itemView.findViewById(R.id.transfer_date);
         mAmount = itemView.findViewById(R.id.transfer_amount);
         mDollar = itemView.findViewById(R.id.transfer_dollar);
+        mIcon = itemView.findViewById(R.id.transfer_icon);
 
         mResources = itemView.getResources();
     }
@@ -33,11 +38,13 @@ public class TransferViewHolder extends RecyclerView.ViewHolder {
             mType.setTextColor(mResources.getColor(R.color.transferReceive));
             mAmount.setTextColor(mResources.getColor(R.color.transferReceive));
             mDollar.setTextColor(mResources.getColor(R.color.transferReceive));
+            Glide.with(mIcon).load(R.drawable.download).into(mIcon);
         } else {
             mType.setText("Send");
             mType.setTextColor(mResources.getColor(R.color.transferSend));
             mAmount.setTextColor(mResources.getColor(R.color.transferSend));
             mDollar.setTextColor(mResources.getColor(R.color.transferSend));
+            Glide.with(mIcon).load(R.drawable.upload).into(mIcon);
         }
 
         mDate.setText(transfer.getDate());
